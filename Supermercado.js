b153c9f1ab8cbbd4470cdeb5d61c167901bdf82e
Sandriski-sanchez <124930632+Sandriski-sanchez@users.noreply.github.com>
2023-05-21 16:58:14 +0200

    Add files via upload

---
+++ Supermercado.js
@@ -0,0 +1,227 @@
+{
+    "Mercadona": [
+      {
+        "direccion": "Calle Concepcion",
+        "codigoPostal": 41019,
+        "coordenadas": "37°41'17.04\"N -5°91'97.46\"O\"",
+        "productos": [
+          {
+            "panaderia": [
+              {
+                "producto": "pan",
+                "marca": "Bimbo",
+                "informacion": {
+                  "unidades": 1,
+                  "fresco": true,
+                  "formato": "envasado",
+                  "cantidad": 500,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 1.99,
+                "perecedero": true,
+                "enOferta": true,
+                "comprado": false
+              },
+              {
+                "producto": "pan semillas y cereales",
+                "marca": "El horno",
+                "informacion": {
+                  "unidades": 1,
+                  "fresco": true,
+                  "formato": "envasado",
+                  "cantidad": 450,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 2.99,
+                "perecedero": true,
+                "enOferta": false,
+                "comprado": true
+              }
+            ],
+            "fruteria": [
+              {
+                "producto": "tomates",
+                "marca": "Mercadona",
+                "informacion": {
+                  "unidades": 1,
+                  "fresco": true,
+                  "formato": "envasado",
+                  "cantidad": 500,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 1.99,
+                "perecedero": true,
+                "enOferta": true,
+                "comprado": true
+              },
+              {
+                "producto": "pimientos",
+                "marca": "Mercadona",
+                "informacion": {
+                  "unidades": 1,
+                  "fresco": true,
+                  "formato": "envasado",
+                  "cantidad": 500,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 0.90,
+                "perecedero": true,
+                "enOferta": false,
+                "comprado": true
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    "Carrefour": [
+      {
+        "direccion": "Calle Jose Jesus Garcia Diaz 1",
+        "codigoPostal": 41020,
+        "coordenadas": "37°40'69.73\"N -5°94'02.48\"O\"",
+        "productos": [
+          {
+            "panaderia": [
+              {
+                "producto": "pan de molde integral",
+                "marca": "Oroweat",
+                "informacion": {
+                  "unidades": 1,
+                  "fresco": true,
+                  "formato": "envasado",
+                  "cantidad": 400,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 3.89,
+                "perecedero": true,
+                "enOferta": true,
+                "comprado": false
+              },
+              {
+                "producto": "pan semillas y cereales",
+                "marca": "Carrefour",
+                "informacion": {
+                  "unidades": 2,
+                  "fresco": true,
+                  "formato": "envasado",
+                  "cantidad": 200,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 0.75,
+                "perecedero": true,
+                "enOferta": false,
+                "comprado": true
+              }
+            ],
+            "fruteria": [
+              {
+                "producto": "fresas",
+                "marca": "Carrefour",
+                "informacion": {
+                  "unidades": 1,
+                  "fresco": true,
+                  "formato": "al peso",
+                  "cantidad": 1,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 4.99,
+                "perecedero": true,
+                "enOferta": true,
+                "comprado": true
+              },
+              {
+                "producto": "aguacate",
+                "marca": "Carrefour",
+                "informacion": {
+                  "unidades": 1,
+                  "fresco": true,
+                  "formato": "bolsa",
+                  "cantidad": 400,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 2.00,
+                "perecedero": true,
+                "enOferta": false,
+                "comprado": true
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    "Alcampo": [
+      {
+        "direccion": "Calle Periodista Jose Antonio Garmendia 3",
+        "codigoPostal": 41020,
+        "coordenadas": "37°40'80.34\"N -5°91'26.21\"O\"",
+        "productos": [
+          {
+            "panaderia": [
+              {
+                "producto": "pan semillas y cereales",
+                "marca": "Alcampo",
+                "informacion": {
+                  "unidades": 1,
+                  "fresco": true,
+                  "formato": "envasado",
+                  "cantidad": 400,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 0.99,
+                "perecedero": true,
+                "enOferta": true,
+                "comprado": true
+              },
+              {
+                "producto": "pan de centeno",
+                "marca": "La cestera",
+                "informacion": {
+                  "unidades": 1,
+                  "fresco": true,
+                  "formato": "envasado",
+                  "cantidad": 400,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 3.99,
+                "perecedero": true,
+                "enOferta": false,
+                "comprado": false
+              }
+            ],
+            "fruteria": [
+              {
+                "producto": "manzanas",
+                "marca": "Alcampo",
+                "informacion": {
+                  "unidades": 1,
+                  "fresco": true,
+                  "formato": "envasado",
+                  "cantidad": 500,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 1.80,
+                "perecedero": true,
+                "enOferta": false,
+                "comprado": false
+              },
+              {
+                "producto": "frambuesa",
+                "marca": "Alcampo",
+                "informacion": {
+                  "unidades": 1,
+                  "fresco": true,
+                  "formato": "al peso",
+                  "cantidad": 200,
+                  "unidadMedida": "gramos"
+                },
+                "precioVta": 2.50,
+                "perecedero": true,
+                "enOferta": false,
+                "comprado": false
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
